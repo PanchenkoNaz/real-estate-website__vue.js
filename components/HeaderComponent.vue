6374f80e8bc4a7a3e6081f1ec81a308ba1836dc7
Nazar Panchenko <120579167+PanchenkoNaz@users.noreply.github.com>
2024-10-30 20:18:39 +0100

    add HeaderComponent.vue

---
+++ components/HeaderComponent.vue
@@ -0,0 +1,26 @@
+<template>
+    <header>
+        <nav>
+            <a href="#">Home</a>
+            <a href="#">Rent</a>
+            <a href="#">Sell</a>
+            <a href="#">Buy</a>
+            <a href="#">About</a>
+        </nav>
+        <button @click="toggleMenu">Menu</button>
+    </header>
+</template>
+
+<script>
+export default {
+    methods: {
+        toggleMenu() {
+            // Логіка для відкриття/закриття меню
+        },
+    },
+};
+</script>
+
+<style scoped>
+/* Стилі для HeaderComponent */
+</style>
