2ea63d37eb960dd01860e351f8159d5df64a568e
Nazar Panchenko <120579167+PanchenkoNaz@users.noreply.github.com>
2024-10-30 20:21:55 +0100

    add FooterComponent.vue

---
+++ components/FooterComponent.vue
@@ -0,0 +1,13 @@
+<template>
+    <footer>
+        <p>&copy; 2023 Wealthome. All rights reserved.</p>
+    </footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+/* Стилі для FooterComponent */
+</style>
